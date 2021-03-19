d32a7254217f0e554fe3870fbf82c66b41ea6560
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2021-03-19 02:00:46 -0600

    Updates

---
+++ precache-manifest.2e4ca585d50dcb98611fa30c8fe6c311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf93b41d04a7daeec6dae8ec1ff2b534",
+    "url": "/react-certification-2020/index.html"
+  },
+  {
+    "revision": "74953d6054aaa29e6ae6",
+    "url": "/react-certification-2020/static/css/main.46641cc8.chunk.css"
+  },
+  {
+    "revision": "c846f75f3466dd75d2d5",
+    "url": "/react-certification-2020/static/js/2.9ec84cd8.chunk.js"
+  },
+  {
+    "revision": "ca47d4a4d3c1fbc7d827954b1710f57e",
+    "url": "/react-certification-2020/static/js/2.9ec84cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74953d6054aaa29e6ae6",
+    "url": "/react-certification-2020/static/js/main.3bd034b9.chunk.js"
+  },
+  {
+    "revision": "d743887fec21cdba8f73",
+    "url": "/react-certification-2020/static/js/runtime-main.bc9b5504.js"
+  }
+]);
