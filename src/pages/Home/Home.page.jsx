0bf4d64e450165ed4a7c42de0a9b21f87ba5b41f
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2021-04-14 01:44:56 -0500

    Home page gets search term from global context

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,21 +1,20 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import './Home.styles.css';
-import { useLocation } from 'react-router-dom';
 import VideosList from '../../components/VideosList';
 import useYoutubeV3 from '../../hooks/useYoutubeV3';
-
-const queryString = require('query-string');
+import { useGlobalContext } from '../../state/GlobalProvider';
 
 function HomePage() {
-  const sectionRef = useRef(null);
-  const { search } = useLocation();
-  const query = queryString.parse(search).q;
-  const url = query
-    ? `search?q=${encodeURIComponent(query)}&part=snippet&type=video&maxResults=25`
+  const { state } = useGlobalContext();
+  const url = state.searchTerm
+    ? `search?q=${encodeURIComponent(
+        state.searchTerm
+      )}&part=snippet&type=video&maxResults=25`
     : 'videos?chart=mostPopular&part=snippet&type=video&maxResults=25';
   const [isLoading, videos] = useYoutubeV3(url, true);
+
   return (
-    <section className="homepage" ref={sectionRef}>
+    <section className="homepage">
       <VideosList videos={videos} isLoading={isLoading} />
     </section>
   );
