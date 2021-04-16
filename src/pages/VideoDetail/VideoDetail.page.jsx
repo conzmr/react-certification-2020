0fda045041e7f1005bd67a9721af8543b934aa41
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2021-04-16 02:53:01 -0500

    Related videos request is about 10 videos

--- src/pages/VideoDetail/VideoDetail.page.jsx
+++ src/pages/VideoDetail/VideoDetail.page.jsx
@@ -7,7 +7,7 @@ import useYoutubeV3 from '../../hooks/useYoutubeV3';
 function VideoDetail() {
   const { id } = useParams();
   const videoDetailUrl = `videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}`;
-  const relatedVideoslUrl = `search?part=snippet&type=video&relatedToVideoId=${id}`;
+  const relatedVideoslUrl = `search?part=snippet&type=video&relatedToVideoId=${id}&format=5&restriction=DE&maxResults=10`;
 
   const [loadingDetail, detail] = useYoutubeV3(videoDetailUrl);
   const [loadingRelatedVideos, relatedVideos] = useYoutubeV3(relatedVideoslUrl, true);
