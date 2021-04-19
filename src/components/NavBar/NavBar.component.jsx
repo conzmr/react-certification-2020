0930e82c301d52f7e087af77476bdfdc20630994
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2021-04-18 23:35:58 -0500

    Logo added to NavBar component

--- src/components/NavBar/NavBar.component.jsx
+++ src/components/NavBar/NavBar.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import AuthMenu from '../AuthMenu';
 import SearchBar from '../SearchBar';
 import ToggleButton from '../ToggleButton';
+import Logo from '../Logo';
+import { ReactComponent as ReactLogo } from '../../assets/logoText.svg';
 
 function NavBar() {
   return (
     <nav className="fixed bg-gray-100 dark:bg-black-900 h-20 w-full flex items-center px-4 justify-between z-50 dark:border-black-100 border-b">
-      <img src="/logo192.png" alt="Logo" className="h-8 w-8" />
+      <Logo/>
       <SearchBar />
       <ToggleButton />
       <AuthMenu />
--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -4,6 +4,7 @@ import { shallow, configure } from 'enzyme';
 import NavBar from './NavBar.component';
 import AuthMenu from '../AuthMenu';
 import SearchBar from '../SearchBar';
+import Logo from '../Logo';
 import ToggleButton from '../ToggleButton';
 
 configure({ adapter: new Adapter() });
@@ -11,6 +12,7 @@ configure({ adapter: new Adapter() });
 describe('Nav bar', () => {
   it('renders containing all its elements: search bar, toggle button and auth menu', () => {
     const wrapper = shallow(<NavBar />);
+    expect(wrapper.containsMatchingElement(<Logo />)).toEqual(true);
     expect(wrapper.containsMatchingElement(<ToggleButton />)).toEqual(true);
     expect(wrapper.containsMatchingElement(<AuthMenu />)).toEqual(true);
     expect(wrapper.containsMatchingElement(<SearchBar />)).toEqual(true);
