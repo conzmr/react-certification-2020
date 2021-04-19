5e96bc2ae997e78a73dafb496c93af4911d14eca
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2021-04-18 23:32:58 -0500

    Auth menu modified to dispatch open login modal action

--- src/components/AuthMenu/AuthMenu.component.jsx
+++ src/components/AuthMenu/AuthMenu.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useAuth } from '../../providers/Auth';
+import { useGlobalContext } from '../../state/GlobalProvider';
 
 function AuthMenu() {
   const history = useHistory();
   const { authenticated, logout } = useAuth();
+  const { dispatch } = useGlobalContext();
 
   function deAuthenticate(event) {
     event.preventDefault();
@@ -12,17 +14,28 @@ function AuthMenu() {
     history.push('/');
   }
 
-  const linkClassName =
-    'text-sm p-1 ml-0.5 text-gray-500 hover:text-gray-900 dark:text-black-50';
+  const showSignInModal = (event) => {
+    event.preventDefault();
+    dispatch({ type: 'OPEN_LOGIN_MODAL' });
+  };
 
-  return authenticated ? (
-    <button type="button" className={linkClassName} onClick={deAuthenticate}>
-      Log out
+  const buttonConfig = authenticated
+    ? {
+        title: 'Log out',
+        onClick: deAuthenticate,
+      }
+    : {
+        title: 'Sign in',
+        onClick: showSignInModal,
+      };
+  return (
+    <button
+      type="button"
+      className="text-base p-1 ml-0.5 text-gray-500 hover:text-gray-900 dark:text-white outline-none focus:outline-none"
+      onClick={buttonConfig.onClick}
+    >
+      {buttonConfig.title}
     </button>
-  ) : (
-    <Link to="/login" className={linkClassName}>
-      Sign in
-    </Link>
   );
 }
 
