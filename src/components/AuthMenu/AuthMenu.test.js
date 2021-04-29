bf885bcfa7be6d393218ea52cab00f70101ca7ea
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2021-04-28 21:55:28 -0500

    Auth menu tests updated

--- src/components/AuthMenu/AuthMenu.test.js
+++ src/components/AuthMenu/AuthMenu.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import AuthMenu from './AuthMenu.component';
 import { useGlobalContext } from '../../state/GlobalProvider';
@@ -8,16 +8,11 @@ jest.mock('../../state/GlobalProvider', () => ({
   useGlobalContext: jest.fn(),
 }));
 
-describe('AuthMenu', () => {
-  beforeEach(() => {
-    useGlobalContext.mockImplementationOnce(() => {
-      return { state: { authenticated: false }, dispatch: jest.fn() };
-    });
-    useGlobalContext.mockImplementationOnce(() => {
-      return { state: { authenticated: true }, dispatch: jest.fn() };
-    });
-  });
+useGlobalContext.mockImplementation(() => {
+  return { state: { authenticated: false, sessionData: {} }, dispatch: jest.fn() };
+});
 
+describe('AuthMenu', () => {
   it('renders login button if user is not authenticated', () => {
     const { container } = render(
       <BrowserRouter>
@@ -27,12 +22,19 @@ describe('AuthMenu', () => {
     expect(container).toHaveTextContent('Sign in');
   });
 
-  it('renders list menu if user is authenticated', () => {
+  it('renders list logout button if user is authenticated', () => {
+    useGlobalContext.mockImplementationOnce(() => {
+      return {
+        state: { authenticated: true, sessionData: { name: 'Test' } },
+        dispatch: jest.fn(),
+      };
+    });
     const { container } = render(
       <BrowserRouter>
         <AuthMenu />
       </BrowserRouter>
     );
+    console.log(screen.debug());
     expect(container).toHaveTextContent('Log out');
   });
 });
